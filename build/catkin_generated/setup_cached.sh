3b54c2d12db4890d650af82891e8137faa18ccce
Vedhas <vedhas.talnikar21@vit.edu>
2023-12-02 13:22:16 +0530

    workspace commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/vedh/learning_ROS/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/noetic/bin:/home/vedh/miniconda3/condabin:/home/vedh/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/vedh/learning_ROS/build'
+export ROS_PACKAGE_PATH="/home/vedh/learning_ROS/src:$ROS_PACKAGE_PATH"
